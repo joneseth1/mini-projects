ed57b7e233181a275bbe3ea07e81ba7275a72347
Seth Jones <joneseth35@gmail.com>
2024-01-08 20:45:52 -0500

    Project setup

---
+++ diy-grep/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+int main() {
+
+	std::cout << "Test12" << std::endl;
+
+}
---
+++ diy-grep/util.sh
@@ -0,0 +1,8 @@
+echo "Building..."
+#build
+g++ -o /c/programs/random-fun/mini-projects/diy-grep/main /c/programs/random-fun/mini-projects/diy-grep/main.cpp
+echo "Done Build and running"
+
+
+#run
+/c/programs/random-fun/mini-projects/diy-grep/main
