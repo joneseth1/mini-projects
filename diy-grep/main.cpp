1826ff3b6826258df73dc7cfd32c041b9111981b
Seth Jones <joneseth35@gmail.com>
2024-01-08 20:58:02 -0500

    Added test sets and intial code

--- diy-grep/main.cpp
+++ diy-grep/main.cpp
@@ -2,11 +2,29 @@
 #include <fstream>
 #include <string>
 
+void grep(const std::string& filename, const std::string& pattern) {
+    std::ifstream file(filename);
+    std::string line;
+    int line_number = 1;
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    while (std::getline(file, line)) {
+        if (line.find(pattern) != std::string::npos) {
+            std::cout << filename << ":" << line_number << ": " << line << std::endl;
+        }
+        line_number++;
+    }
+
+    file.close();
+}
 
 
-
-int main() {
+int main(int argc, char* argv[]) {
 
 	std::cout << "Test12" << std::endl;
-
+	return 0;
 }
