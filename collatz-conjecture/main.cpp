54edaf64a9521abcef0c893da6e2bdc21f5c55b9
Seth Jones <joneseth35@gmail.com>
2024-01-13 19:52:09 -0500

    Setting up the basics

--- collatz-conjecture/main.cpp
+++ collatz-conjecture/main.cpp
@@ -2,6 +2,22 @@
 
 
 
+ // Collatz Conjecture
+void collatz(unsigned int n) {
+    while (n != 1) {
+        std::cout << n << " ";
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    std::cout << "1\n";
+}
+ 
+
+
+
 
 int main(){
 	return 0; 
