0f531d261818a72e5590ec1536629a0d6a00a6bb
Seth Jones <joneseth35@gmail.com>
2024-01-17 16:07:24 -0500

    Added Project Euler dir for those problems, finished the first one. Fun!

---
+++ puzzels/project_euler/util.sh
@@ -0,0 +1,8 @@
+echo "Building..."
+#build
+g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/multiples_of_3_or_5 /c/programs/random-fun/mini-projects/puzzels/project_euler/multiples_of_3_or_5.cpp
+echo "Done Build and running"
+
+
+#run
+/c/programs/random-fun/mini-projects/puzzels/project_euler/multiples_of_3_or_5
