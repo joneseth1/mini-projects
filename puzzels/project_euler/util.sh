b5b3ef6498756b31186515f2749b1701ed1c09a3
Seth Jones <joneseth35@gmail.com>
2024-01-26 07:47:49 -0500

    Largest prime factor.

--- puzzels/project_euler/util.sh
+++ puzzels/project_euler/util.sh
@@ -1,8 +1,8 @@
 echo "Building..."
 #build
-g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/even_fibonacci_numbers /c/programs/random-fun/mini-projects/puzzels/project_euler/even_fibonacci_numbers.cpp
+g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor.cpp
 echo "Done Build and running"
 
 
 #run
-/c/programs/random-fun/mini-projects/puzzels/project_euler/even_fibonacci_numbers
+/c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor
