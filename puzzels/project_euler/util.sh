94978691cbab486591a08610132ca9314916f8d4
Seth Jones <joneseth35@gmail.com>
2024-01-27 13:24:52 -0500

    project Euler problem #4.

--- puzzels/project_euler/util.sh
+++ puzzels/project_euler/util.sh
@@ -1,8 +1,8 @@
 echo "Building..."
 #build
-g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor.cpp
+g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_palidrome_product /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_palidrome_product.cpp
 echo "Done Build and running"
 
 
 #run
-/c/programs/random-fun/mini-projects/puzzels/project_euler/largest_prime_factor
+/c/programs/random-fun/mini-projects/puzzels/project_euler/largest_palidrome_product
