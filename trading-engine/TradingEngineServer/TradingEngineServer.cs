52e640d7cb1d0c69ee663520a5315209d0d1f9fa
Seth Jones <joneseth35@gmail.com>
2024-01-28 11:43:24 -0500

    Setting up the server as background service.

---
+++ trading-engine/TradingEngineServer/TradingEngineServer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core
+{
+    class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        private readonly ILogger<TradingEngineServer> _logger;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+
+            }
+        }
+    }
+}
