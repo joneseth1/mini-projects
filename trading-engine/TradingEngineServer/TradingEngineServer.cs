4c04174db1e3c51ec4bc3d62ce5320abbceda879
Seth Jones <joneseth35@gmail.com>
2024-01-28 11:51:09 -0500

    Injected server config

--- trading-engine/TradingEngineServer/TradingEngineServer.cs
+++ trading-engine/TradingEngineServer/TradingEngineServer.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using TradingEngineServer.Core.Configuration;
 
 namespace TradingEngineServer.Core
 {
     class TradingEngineServer : BackgroundService, ITradingEngineServer
     {
         private readonly ILogger<TradingEngineServer> _logger;
-        public TradingEngineServer(ILogger<TradingEngineServer> logger)
+        private readonly TradingEngineServerConfiguration _tradingEngineServerConfig;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<TradingEngineServerConfiguration> config)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tradingEngineServerConfig = config.Value ?? throw new ArgumentNullException(nameof(config));
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
