79a470ad225e60379af35aebf5e674a73fc7a482
Seth Jones <joneseth35@gmail.com>
2024-01-30 11:01:58 -0500

    Trying to increase accuracy.

--- titanic/scripts/titanic.jl
+++ titanic/scripts/titanic.jl
@@ -1,26 +1,27 @@
-using DecisionTree, ScikitLearn.CrossValidation, DataFrames, CSV, Random, Statistics
+using DataFrames, CSV, DecisionTree, Random, Statistics
+using ScikitLearn.CrossValidation: KFold, cross_val_score
 
 # Turn CSV => DataFrame
 train_df = CSV.read("data/train.csv", DataFrame)
 
 # Drop missing values and replace missing Ages with the median
-train_df = dropmissing(train_df, "Embarked")
-train_df.Age = coalesce.(train_df.Age, 28.0)
-train_df = select(train_df, Not("Cabin"))
+dropmissing!(train_df, "Embarked")
+train_df.Age = coalesce.(train_df.Age, median(skipmissing(train_df.Age)))
+train_df = select(train_df, Not(:Cabin))
 
 # Select relevant columns
-train_df = select(train_df, Not(["PassengerId", "Name", "Ticket"]))
+train_df = select(train_df, Not([:PassengerId, :Name, :Ticket]))
 
 # Convert Embarked and Sex to numerical values
 train_df.Embarked = map(x -> x == "S" ? 1 : (x == "C" ? 2 : 3), train_df.Embarked)
 train_df.Sex = map(x -> x == "female" ? 1 : 2, train_df.Sex)
 
 # Split the training set into features (X) and labels (y)
-y = train_df[:, "Survived"]
-X = Matrix(train_df[:, Not(["Survived"])])
+y = train_df[:, :Survived]
+X = convert(Matrix, train_df[:, Not(:Survived)])
 
 # Train a Random Forest classifier and print accuracy
-model = RandomForestClassifier(n_trees=100, min_samples_split=10, max_depth=10)
+model = DecisionTree.RandomForestClassifier(n_trees=100, min_samples_split=10, max_depth=10)
 kf = KFold(size(X, 1), n_folds=5, shuffle=true, random_state=42)
 accuracy = cross_val_score(model, X, y, cv=kf)
 
