1686fcc4036f9bc70eb75e8b873e720cf87de165
Seth Jones <joneseth35@gmail.com>
2024-02-04 16:00:57 -0500

    I finally got the answer correct, minmial change in my code but added smalllestDivisble function

--- puzzels/project_euler/smallest_multiple.cpp
+++ puzzels/project_euler/smallest_multiple.cpp
@@ -15,9 +15,17 @@ int lcm(int a, int b) {
     return (a * b) / gcd(a, b);
 }
 
+// Function to find the smallest positive number evenly divisible by all numbers from 1 to n
+int smallestDivisible(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result = lcm(result, i);
+    }
+    return result;
+}
 
 int main() {
-    int n = 20;
+    int n = 19;
     std::cout << "The smallest positive number evenly divisible by all numbers from 1 to " << n << " is: ";
     std::cout << smallestDivisible(n) << std::endl;
     return 0;
