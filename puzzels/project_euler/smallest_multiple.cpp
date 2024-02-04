a749076df3b9e4d360e6c41b8d59bd6367f7e6ef
Seth Jones <joneseth35@gmail.com>
2024-02-04 15:54:59 -0500

    New attempt at a lcm function

--- puzzels/project_euler/smallest_multiple.cpp
+++ puzzels/project_euler/smallest_multiple.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 
-// Function to calculate the GCD of two numbers
+// Function to calculate the greatest common divisor (GCD) using Euclid's algorithm
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-// Function to calculate the LCM of two numbers
+// Function to calculate the least common multiple (LCM)
 int lcm(int a, int b) {
     return (a * b) / gcd(a, b);
 }
 
-// Function to calculate the LCM of numbers from 1 to n
-int lcmOfRange(int n) {
-    int result = 1;
-    for (int i = 2; i <= n; ++i) {
-        result = lcm(result, i);
-    }
-    return result;
-}
 
 int main() {
     int n = 20;
     std::cout << "The smallest positive number evenly divisible by all numbers from 1 to " << n << " is: ";
-    std::cout << lcmOfRange(n) << std::endl;
+    std::cout << smallestDivisible(n) << std::endl;
     return 0;
 }
