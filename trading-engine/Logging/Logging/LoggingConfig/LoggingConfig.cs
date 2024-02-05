bc89dc041c5d02141837327a5642ed39b48063e1
Seth Jones <joneseth35@gmail.com>
2024-02-05 16:55:17 -0500

    Updated appsettings.json

---
+++ trading-engine/Logging/Logging/LoggingConfig/LoggingConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Logging.LoggingConfig
+{
+    public class LoggingConfig
+    {
+        public LoggerType LoggerType { get; set; }
+        public TextLoggerConfig TextLoggerConfig { get; set; }
+
+    }
+
+    public class TextLoggerConfig
+    {
+        public string Directory { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+
+    }
+
+}
