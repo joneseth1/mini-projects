c1e382c9fdee6e3a78e36ab3088f5330749f2dbf
Seth Jones <joneseth35@gmail.com>
2024-02-08 09:13:28 -0500

    Implemented thread safe queue.

--- trading-engine/Logging/Logging/TextLogger.cs
+++ trading-engine/Logging/Logging/TextLogger.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.Extensions.Options;
 using TradingEngineServer.Logging.LoggingConfig;
 
@@ -12,9 +14,14 @@ namespace TradingEngineServer.Logging
         private readonly LoggerConfig _loggingConfig;
         public TextLogger(IOptions<LoggerConfig> loggingConfig) : base()
         {
-            _loggingConfig = loggingConfig .Value ?? throw new ArgumentNullException(nameof(loggingConfig));
+            _loggingConfig = loggingConfig.Value ?? throw new ArgumentNullException(nameof(loggingConfig));
         }
-        protected override void Log(LogLevel loglevel, string module, string message) => throw new NotImplementedException();
+        protected override void Log(LogLevel loglevel, string module, string message)
+        {
+            _logQueue.Post(new LogInformation(loglevel,module,message,DateTime.Now, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
+        }
         public void Dispose() => throw new NotImplementedException();
+
+        private readonly BufferBlock<LogInformation> _logQueue = new BufferBlock<LogInformation>();
     }
 }
