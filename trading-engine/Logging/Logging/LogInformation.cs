e0c5a13ab7f5271f54a90b3cda23470d4e4a63f2
Seth Jones <joneseth35@gmail.com>
2024-02-08 08:04:55 -0500

    Started implementing LogInformation record and found a fix to the vscode 2022 problem.

---
+++ trading-engine/Logging/Logging/LogInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Logging
+{
+   public record LogInformation(LogLevel loglevel, string Modules, string Message, DateTime Now, int ThreadId, string ThreadName);
+
+}
+
+
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { };
+}
