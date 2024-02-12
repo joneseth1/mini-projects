439aae4d2f704f1cc06a3f16a13542fb1422b07c
Seth Jones <joneseth35@gmail.com>
2024-02-12 14:52:32 -0500

    Implemented IOrderCore.

---
+++ trading-engine/Orders/IOrderCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TradingEgnineServer.Orders
+{
+    public interface IOrderCore
+    {
+        public long OrderId { get; }
+        public string Username { get; }
+        public int SecurityId { get; }
+    }
+}
