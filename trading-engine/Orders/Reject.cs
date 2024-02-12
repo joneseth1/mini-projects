610990c5a0315f166068553902d3cb31cadbb766
Seth Jones <joneseth35@gmail.com>
2024-02-12 16:17:30 -0500

    Added rejects

---
+++ trading-engine/Orders/Reject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Rejects
+{
+    public class Reject : IOrderCore
+    {
+        public Reject(IOrderCore rejectedOrder, RejectReason rejectReason)
+        {
+            _orderCore = rejectedOrder;
+            RejectReason = rejectReason;
+        }
+        public RejectReason RejectReason { get; private set; }
+        public long OrderId => _orderCore.OrderId;
+
+        public string Username => _orderCore.Username;
+
+        public int SecurityId => _orderCore.SecurityId;
+
+
+        private readonly IOrderCore _orderCore;
+    }
+}
---
+++ trading-engine/Orders/RejectCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Rejects
+{
+    public sealed class RejectCreator
+    {
+        public static Reject GenerateOrderCoreReject(IOrderCore rejectOrder, RejectReason rejectReason)
+        {
+            return new Reject(rejectOrder, rejectReason);
+        }
+    }
+}
