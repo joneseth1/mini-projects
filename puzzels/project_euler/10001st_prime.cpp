4104ce1687ed3995a7dcbeb4729b1eb02c1a3bc5
Seth Jones <joneseth35@gmail.com>
2024-02-13 13:47:54 -0500

    Intital approach.

--- puzzels/project_euler/10001st_prime.cpp
+++ puzzels/project_euler/10001st_prime.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <cmath>
 
-
-
-
-int main() 
+bool is_prime(int n)
 {
+    if (n <= 1) {
+        return false; // 0 and 1 are not prime 
+    }
+    
+    int sqr_root = sqrt(n);
+    for (int i = 2; i <= sqr_root; i++)
+    {
+        if (n % i == 0)
+        {
+            return false; // n is divisible by i, hence not a prime
+        }
+    }
+    return true; // if no factor found, n is prime
+}
 
-	return 0;
+int main()
+{
+    int number = 25;
+    if (is_prime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is not a prime number." << std::endl;
+    }
+
+    return 0;
 }
