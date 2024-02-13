0c697b1f6986b617b1473decb0c2a7bdf1965946
Seth Jones <joneseth35@gmail.com>
2024-02-13 13:34:13 -0500

    Intial commit for 10001st_prime

--- puzzels/project_euler/util.sh
+++ puzzels/project_euler/util.sh
@@ -1,8 +1,8 @@
 echo "Building..."
 #build
-g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/sum_square_difference /c/programs/random-fun/mini-projects/puzzels/project_euler/sum_square_difference.cpp
+g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/10001st_prime /c/programs/random-fun/mini-projects/puzzels/project_euler/10001st_prime.cpp
 echo "Done Build and running"
 
 
 #run
-/c/programs/random-fun/mini-projects/puzzels/project_euler/sum_square_difference
+/c/programs/random-fun/mini-projects/puzzels/project_euler/10001st_prime
