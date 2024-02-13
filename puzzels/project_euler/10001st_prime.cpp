e842d37d17017deb12fcc4026c2c8a4f84d1d23d
Seth Jones <joneseth35@gmail.com>
2024-02-13 14:26:46 -0500

    Finished up the puzzel.

--- puzzels/project_euler/10001st_prime.cpp
+++ puzzels/project_euler/10001st_prime.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
-bool is_prime(int n)
-{
-    if (n <= 1) {
-        return false; // 0 and 1 are not prime 
-    }
-    
-    int sqr_root = sqrt(n);
-    for (int i = 2; i <= sqr_root; i++)
-    {
-        if (n % i == 0)
-        {
-            return false; // n is divisible by i, hence not a prime
+std::vector<int> prime;
+
+void generate_primes(int limit) {
+    for (int n = 2; prime.size() < limit; ++n) {
+        bool is_prime = true;
+        int sqr_root = sqrt(n);
+        for (int i = 2; i <= sqr_root; ++i) {
+            if (n % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) {
+            prime.push_back(n);
         }
     }
-    return true; // if no factor found, n is prime
 }
 
-int main()
-{
-    int number = 25;
-    if (is_prime(number)) {
-        std::cout << number << " is a prime number." << std::endl;
-    } else {
-        std::cout << number << " is not a prime number." << std::endl;
-    }
+int main() {
+    int prime_index = 10001; 
+    generate_primes(prime_index); 
+    
+    std::cout << "The " << prime_index << "st prime number is: " << prime.back() << std::endl;
 
     return 0;
 }
