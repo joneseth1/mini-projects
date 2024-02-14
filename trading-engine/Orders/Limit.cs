fe092738ec7e8abaa7857e9bc6ebfef2dd4ebf14
Seth Jones <joneseth35@gmail.com>
2024-02-14 15:15:11 -0500

    IMplmented gets for level order count and quantity

--- trading-engine/Orders/Limit.cs
+++ trading-engine/Orders/Limit.cs
@@ -10,11 +10,48 @@ namespace TradingEngineServer.Orders
         public OrderbookEntry Head { get; set; }
         public OrderbookEntry Tail{ get; set; }
 
+        public uint GetLevelOrderCount()
+        {
+            uint orderCount = 0; 
+            OrderbookEntry head = Head;
+            while (head != null)
+            {
+                if (head.CurrentOrder.CurrentQuantity != 0)
+                    orderCount++;
+                head = head.Next;
+            }
+            return orderCount;
+        }
+
+        public uint GetLevelOrderQuantity()
+        {
+            uint quantity = 0;
+            OrderbookEntry head = Head;
+            while (head != null)
+            {
+                quantity += head.CurrentOrder.CurrentQuantity;
+                head = head.Next;
+            }
+            return quantity;
+        }
+
         public bool IsEmpty
         {
             get
             {
                 return Head == null && Tail == null;
+            }
+        }
+        public Side Side
+        {
+            get
+            {
+                if(IsEmpty)
+                    return Side.Unkown;
+                else
+                {
+                    return Head.CurrentOrder.IsBuySide ? Side.Bid : Side.Ask;
+                }
             }
         }
     }
