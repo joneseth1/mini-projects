abd261a92e303a8c3dcd9421fb4ca670ddccf844
Seth Jones <joneseth35@gmail.com>
2024-02-14 11:27:56 -0500

    Added OrderbookEntry.

---
+++ trading-engine/Orders/OrderbookEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    class Limit
+    {
+
+    }
+
+    class OrderbookEntry
+    {
+        public OrderbookEntry(Order currentOrder, Limit parentLimit)
+        {
+            CreationTime = DateTime.Now;
+            ParentLimit = parentLimit;
+            CurrentOrder = currentOrder;
+
+        }
+        public DateTime CreationTime { get; private set; }
+        public Order CurrentOrder { get; private set; }
+        public Limit ParentLimit { get; private set; }
+        public OrderbookEntry Next { get; private set; }
+        public OrderbookEntry Previous { get; private set; }
+
+    }
+}
