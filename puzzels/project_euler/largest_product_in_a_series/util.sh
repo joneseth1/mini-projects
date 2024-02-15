8b41fe484aa4ce6c88e40874fccf04d708abf97e
Seth Jones <joneseth35@gmail.com>
2024-02-15 09:44:21 -0500

    Intital setup for largest_prduct_in_a_series.cpp

---
+++ puzzels/project_euler/largest_product_in_a_series/util.sh
@@ -0,0 +1,8 @@
+echo "Building..."
+#build
+g++ -o /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_product_in_a_series/largest_product_in_a_series /c/programs/random-fun/mini-projects/puzzels/project_euler/largest_product_in_a_series/largest_product_in_a_series.cpp
+echo "Done Build and running"
+
+
+#run
+/c/programs/random-fun/mini-projects/puzzels/project_euler/largest_product_in_a_series/largest_product_in_a_series
