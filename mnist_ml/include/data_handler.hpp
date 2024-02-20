a33395014292832ec2b09dfb2849c554cecbdcfc
Seth Jones <joneseth35@gmail.com>
2024-02-20 12:34:24 -0500

    Finished implemented the gets for the training,test,val.

--- mnist_ml/include/data_handler.hpp
+++ mnist_ml/include/data_handler.hpp
@@ -22,6 +22,30 @@ class data_handler
     int num_classes; 
     int feature_vector_size;
     std::map<uint8_t, int> class_map;
+
+    const double TRAIN_SET_PERCENT = 0.75;
+    const double TRAIN_SET_PERCENT = 0.20;
+    const double VALIDATION_PERCENT = 0.05;
+
+    public: 
+    data_handler();
+    ~data_handler();
+
+    void read_feature_vector(std::string path);
+    void read_feature_labels(std::string path);
+    void split_data();
+    void count_classes();
+
+    uint32_t convert_to_little_endian(const unsigned char* bytes);
+
+
+    std::vector<data *> * get_training_data();
+    std::vector<data *> * get_test_data();
+    std::vector<data *> * get_validation_data();
+
+
+
+
 };
 
 
