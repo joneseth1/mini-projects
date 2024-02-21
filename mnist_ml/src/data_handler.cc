87c48a02c9fc76fe77609f5f603215230fb23778
Seth Jones <joneseth35@gmail.com>
2024-02-21 18:24:09 -0500

    More makings of data_handler.

--- mnist_ml/src/data_handler.cc
+++ mnist_ml/src/data_handler.cc
@@ -10,7 +10,29 @@ data_handler::data_handler()
 
 }
 
+data_handler::~data_handler()
+{
+
+}
+
 void data_handler::read_feature_vector(std::string path)
+{
+    uint32_t head[4]; 
+    unsigned char bytes[4];
+    FILE *f = fopen(path.c_str(), "r");
+    if(f)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if(fread(bytes, sizeof(bytes), 1, f))
+            {
+                head[i] = convert_to_little_endian(bytes);
+            }
+
+        }
+
+    }
+}
 
 void read_feature_vector(std::string path);
 void read_feature_labels(std::string path);
