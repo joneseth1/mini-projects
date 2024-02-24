e2d36a0d15b344ac084d33e893209805624e2650
Seth Jones <joneseth35@gmail.com>
2024-02-24 10:50:51 -0500

    Finished problem 10.

--- puzzels/project_euler/summation_of_primes.cpp
+++ puzzels/project_euler/summation_of_primes.cpp
@@ -2,51 +2,28 @@
 #include <vector>
 #include <cmath>
 
-// int PRIME_SUM = 0;
-
-// bool isPrime(int num) {
-//     if (num <= 1) {
-//         return false;
-//     }
-//     for (int i = 2; i * i <= num; i++) {
-//         if (num % i == 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-
-void magic() {
-    double n = -100; 
-    std::vector<int> num_list; 
-    bool bol = true;
-
-
-    while(bol) {
-        bol = true;
-        n += 1; 
-        num_list.push_back(n);
-        if (n == 100)
-        {
-            bol = false;
-        }
-        std::cout << n << std::endl;
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
     }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-
 int main() {
-    // for (int i = 0; i < 2000000; i++) {
-    //     if (isPrime(i)) {
-    //         PRIME_SUM += i;
-    //     }
-    // }
+    long long PRIME_SUM = 0; // Use long long to handle larger sums
 
-    // std::cout << PRIME_SUM << std::endl;
+    for (int i = 2; i < 2000000; i++) { // Start from 2, as 0 and 1 are not prime
+        if (isPrime(i)) {
+            PRIME_SUM += i;
+        }
+    }
 
+    std::cout << PRIME_SUM << std::endl;
 
-
-    magic();
     return 0;
 }
