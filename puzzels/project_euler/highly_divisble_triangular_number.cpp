b693a2483d95038afa1cf1c8d9c04e2d6e76494b
Seth Jones <joneseth35@gmail.com>
2024-02-24 11:35:14 -0500

    Finshed up #10, need to find a more optmized solution

--- puzzels/project_euler/highly_divisble_triangular_number.cpp
+++ puzzels/project_euler/highly_divisble_triangular_number.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 
-
-
-
-int main()
-{
-    
-    int N = 6; 
-    int j = N;
-    int sum = N;
-
-    while(j != 1)
-    {   
-        j = j - 1;
-        sum += j;
+// Function to calculate the number of divisors
+int countDivisors(long long n) {
+    int divisors = 0;
+    for (long long i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            divisors += (i * i == n) ? 1 : 2;
+        }
     }
+    return divisors;
+}
 
-    std::cout << sum << std::endl;
+int main() {
+    long long triangular_number = 0;
+    int i = 1;
+
+    while (true) {
+        triangular_number += i;
+        int divisors = countDivisors(triangular_number);
+        if (divisors > 500) {
+            std::cout << "The first triangular number with over 500 divisors is: " << triangular_number << std::endl;
+            break;
+        }
+        ++i;
+    }
 
     return 0;
 }
-
 
 
