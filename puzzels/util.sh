ff5ccc9b1fda12b74031b01cfd20ae96e9643f2b
Seth Jones <joneseth35@gmail.com>
2024-02-25 20:08:11 -0500

    random urge to do a merge sort.

---
+++ puzzels/util.sh
@@ -0,0 +1,8 @@
+echo "Building..."
+#build
+g++ -o /c/programs/random-fun/mini-projects/puzzels/merge_sort /c/programs/random-fun/mini-projects/puzzels/merge_sort.cpp
+echo "Done Build and running"
+
+
+#run
+/c/programs/random-fun/mini-projects/puzzels/merge_sort
