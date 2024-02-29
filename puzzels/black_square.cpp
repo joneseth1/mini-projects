061f1788753f44fd1bd3aba4e8cb608c8b307e46
Seth Jones <joneseth35@gmail.com>
2024-02-29 18:38:41 -0500

    setting up the destroy for the window.

--- puzzels/black_square.cpp
+++ puzzels/black_square.cpp
@@ -1,11 +1,43 @@
-#include <iostream>
+#include <windows.h>
+#include <algorithm>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Register the window class
+    const char CLASS_NAME[] = "BlackSquareWindowClass";
+
+    WNDCLASS wc = {};
 
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
 
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
 
-int main () 
-{
+            
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+            int squareSize = std::min(width, height) / 2; 
+            int x = (width - squareSize) / 2;
+            int y = (height - squareSize) / 2;
 
+            RECT squareRect = { x, y, x + squareSize, y + squareSize };
+            FillRect(hdc, &squareRect, (HBRUSH)GetStockObject(BLACK_BRUSH));
 
-    return 0; 
+        }
+        return 0;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
 }
--- puzzels/util.sh
+++ puzzels/util.sh
@@ -1,8 +1,8 @@
 echo "Building..."
 #build
-g++ -o /c/programs/random-fun/mini-projects/puzzels/merge_sort /c/programs/random-fun/mini-projects/puzzels/merge_sort.cpp
+g++ -o /c/programs/random-fun/mini-projects/puzzels/black_square /c/programs/random-fun/mini-projects/puzzels/black_square.cpp
 echo "Done Build and running"
 
 
 #run
-/c/programs/random-fun/mini-projects/puzzels/merge_sort
+/c/programs/random-fun/mini-projects/puzzels/black_square
