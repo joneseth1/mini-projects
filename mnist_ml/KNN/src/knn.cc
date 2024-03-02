f1daeaeb6ff3286a9f9439e534cde2dee575b432
Seth Jones <joneseth35@gmail.com>
2024-03-02 10:40:40 -0500

    More work on knn.cc

--- mnist_ml/KNN/src/knn.cc
+++ mnist_ml/KNN/src/knn.cc
@@ -7,28 +7,57 @@
 
 
 
-knn::knn(int)
+knn::knn(int val)
 {
     k = val;
 }
 
 knn::knn()
 {
-    //just need to be declared 
+    //default constructor   
 }
 
 knn::~knn()
 {
-    //just need to be declared 
+    //default destructor  
 }
 
 void knn::find_knearest(data *query_point);
-void knn::set_training_data(std::vector<data *> *vect);
-void knn::test_data(std::vector<data *> *vect);
-void knn::validation_data(std::vector<data *> *vect);
-void knn::set_k(int val);
+
+void knn::set_training_data(std::vector<data *> *vect)
+{
+    training_data = vect;
+}
+
+void knn::test_data(std::vector<data *> *vect)
+{
+    test_data = vect;
+}
+
+void knn::validation_data(std::vector<data *> *vect)
+{
+    validation_data = vect;
+}
+
+void knn::set_k(int val)
+{
+    k = val;
+}
 
 int knn::predict();
-double knn::calc_distance(data* query_point, data* input);
+
+double knn::calc_distance(data* query_point, data* input)
+{
+    double distance = 0.0;
+#ifdef EUCLID
+    for(unsigned i = 0; i < query_point->get_feature_vector(); i++)
+    {
+        distance = pow(query_point->get_feature_vector()->at(i) - input->get_feature_vector()->at(i), 2);
+    }
+    distance = sqrt(distance);
+#elif defined MANHATTAN
+return distance;
+}
+
 double knn::validate_performance();
 double knn::test_performance();
