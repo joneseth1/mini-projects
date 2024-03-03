b161d4e0f9a35a824e05f163270e4f8ee83e29ab
Seth Jones <joneseth35@gmail.com>
2024-03-03 17:26:23 -0500

    A little more optimal.

--- puzzels/proposition_19.cc
+++ puzzels/proposition_19.cc
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-int main()
-{
-    std::vector<std::vector<int>> divisors(201);
+int main() {
+    const int MAX_NUMBER = 200;
+    std::vector<std::vector<int>> divisors(MAX_NUMBER + 1);
 
-    for (int i = 1; i <= 200; ++i)
-    {
-        for (int j = 1; j <= 200; ++j)
-        {
-            if (i % j == 0)
-            {
+    for (int i = 1; i <= MAX_NUMBER; ++i) {
+        int limit = std::sqrt(i);
+        for (int j = 1; j <= limit; ++j) {
+            if (i % j == 0) {
                 divisors[i].push_back(j);
+                if (j != i / j) {
+                    divisors[i].push_back(i / j);
+                }
             }
         }
     }
 
     // Displaying the divisors
-    for (int i = 1; i <= 200; ++i)
-    {
+    for (int i = 1; i <= MAX_NUMBER; ++i) {
         std::cout << "Divisors of " << i << ": ";
-        for (int divisor : divisors[i])
-        {
+        for (int divisor : divisors[i]) {
             std::cout << divisor << " ";
         }
         std::cout << std::endl;
