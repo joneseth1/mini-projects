a44240048d3c6a348ded54fb4965687e3e91a007
Seth Jones <joneseth35@gmail.com>
2024-03-11 05:18:58 -0400

    Working in add_to_cluster.

--- mnist_ml/KMEANS/include/kmeans.hpp
+++ mnist_ml/KMEANS/include/kmeans.hpp
@@ -18,24 +18,45 @@ typedef struct cluster
     std::map<int, int> class_counts;
     int most_frequent_class;
 
-    cluster(data *initail_point)
+    cluster(data *initial_point)
     {
         centroid = new std::vector<double>;
         cluster_points = new std::vector<data *> ;
 
-        for(auto value : *(initail_point->get_feature_vector()))
+        for(auto value : *(initial_point->get_feature_vector()))
         {
             centroid->push_back(value);
         }
 
-        cluster_points->push_back(initail_point);
-        class_counts[initail_point->get_label()] = 1;
-        most_frequent_class = initail_point->get_label();
+        cluster_points->push_back(initial_point);
+        class_counts[initial_point->get_label()] = 1;
+        most_frequent_class = initial_point->get_label();
     }
 
     void add_to_cluster(data *point)
     {
-        
+        int previous_size = cluster_points->size();
+
+        cluster_points->push_back(point);
+        for(int i = 0; i < centroid->size() - 1; i++)
+        {
+            double value = centroid->at(i);
+            value *= previous_size;
+            value += point->get_feature_vector_size();
+            
+            //TODO Need to test 
+            value /= (double)cluster_points->size();
+            centroid->at(i) = value;
+        }
+
+        if(class_counts->find(point->get_label()) == class_counts->end())
+        {
+            class_counts[point->get_label()] = 1;
+        } else 
+        {
+            class_counts[point->get_label()]++;
+        }
+
     }
 
 } cluster_t;
