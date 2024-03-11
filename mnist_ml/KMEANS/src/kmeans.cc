3aa8bab7ec718b9caee160f1fdfb1e9b635a0b7e
Seth Jones <joneseth35@gmail.com>
2024-03-11 05:47:30 -0400

    Started implementing clusters

--- mnist_ml/KMEANS/src/kmeans.cc
+++ mnist_ml/KMEANS/src/kmeans.cc
@@ -21,8 +21,38 @@ void kmeans::init_clusters()
         used_indexes->insert(index);
     }
 }
-void kmeans::init_clusters_for_each_class();
-void kmeans::train();
-double kmeans::euclidean_distance(std::vector<double> *, data *);
-double kmeans::validate();
-double kmeans::test();
+
+void kmeans::init_clusters_for_each_class()
+{
+    std::unordered_set<int> classes_used;
+    for(int i = 0; i < training_data->size(); i++)
+    {
+        if(classes_used.find(training_data->at(i)->get_label()) == classes_used.end())
+        {
+            clusters->push_back(new cluster_t(training_data->at(i)));
+            classes_used.insert(training_data->at(i)->get_label());
+            used_indexes->insert(i);
+        }
+    }
+
+}
+
+void kmeans::train()
+{
+
+}
+
+double kmeans::euclidean_distance(std::vector<double> *, data *)
+{
+
+}
+
+double kmeans::validate()
+{
+
+}
+
+double kmeans::test()
+{
+    
+}
