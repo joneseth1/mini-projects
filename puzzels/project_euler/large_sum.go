7e8e431931e373510a930a0c5d5cf456b15c6707
Seth Jones <joneseth35@gmail.com>
2024-03-14 14:27:30 -0400

    readding the file

--- puzzels/project_euler/large_sum.go
+++ puzzels/project_euler/large_sum.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 
-	fmt.Println("TEST")
+	file, err := os.Open("numbers.txt")
+	if err != nil {
+		fmt.Println("Can't open file: ", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var numbers []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println("Error converting line to integer:", err)
+			continue
+		}
+
+		numbers = append(numbers, number)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error readding file: ", err)
+	}
 }
